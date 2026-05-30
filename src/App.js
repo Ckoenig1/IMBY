17d09b9011217120287d4a218fa8ea695f94c0e2
Chris Koenig <ckoenig1@umd.edu>
2026-05-29 18:01:30 -0700

    menu bar additions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cow from './cow.jpg'
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
+      <div className="topbar">
+        
+          <button className="hamburger" onClick={() => setOpen(!open)} aria-label="menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+     
+
+      
+         
+            <div className='nav-links'>Music</div>
+            <div className='nav-links'>Camping & Lodging</div>
+            <div className="nav-center">
+              <img src={cow} alt="cow" className="center-logo" />
+            </div>
+            <div className='nav-links'>Beyond The Music</div>
+            <div className='nav-links'>Get Involved</div>
+
+          <a className="buy-button" href="#">BUY TICKETS</a>
+      
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cow} style={{ padding: "100px" }} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
