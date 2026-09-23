87591415e0e90c3441a64c959d34de916314c12b
Chris Koenig <ckoenig1@umd.edu>
2026-09-23 15:27:34 -0400

    menu item images

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import If from './If';
 import lineup from '../assets/lineup.jpg';
+import campingImage from '../assets/menu-images/camping.jpg';
+import beyondTheMusicImage from '../assets/menu-images/beyond-the-music.jpg';
+import getInvolvedImage from '../assets/menu-images/get-involved.jpg';
 const DropDown = (props) => {
     const select = (section) => props.onSelect(section);
     return(
@@ -13,27 +16,23 @@ const DropDown = (props) => {
                         <button className="dropdown-item" onClick={() => select('music')}>
                             <img className="dropdown-image" src={lineup} alt="Lineup" />
                             <div className="dropdown-text">Lineup</div>
-                                                </button>
-                                                    <button className="dropdown-item" onClick={() => select('music')}>
-                            <img className="dropdown-image" src={lineup} alt="Lineup" />
-                            <div className="dropdown-text">Schedule</div>
                         </button>
                     </If>
                     <If condition={props.displayType === "Camping & Lodging"}>
                         <button className="dropdown-item" onClick={() => select('tickets')}>
-                            <img className="dropdown-image" src={lineup} alt="Camping & Lodging" />
+                            <img className="dropdown-image" src={campingImage} alt="Camping & Lodging" />
                             <div className="dropdown-text">Camping & Lodging</div>
                         </button>
                     </If>  
                         <If condition={props.displayType === "Beyond The Music"}>
                         <button className="dropdown-item" onClick={() => select('beyond')}>
-                            <img className="dropdown-image" src={lineup} alt="Beyond The Music" />
+                            <img className="dropdown-image" src={beyondTheMusicImage} alt="Beyond The Music" />
                             <div className="dropdown-text">Beyond The Music</div>
                         </button>
                     </If>
                     <If condition={props.displayType === "Get Involved"}>
                         <button className="dropdown-item" onClick={() => select('involved')}>
-                            <img className="dropdown-image" src={lineup} alt="Get Involved" />
+                            <img className="dropdown-image" src={getInvolvedImage} alt="Get Involved" style={{ objectPosition: 'center 32%' }} />
                             <div className="dropdown-text">Get Involved</div>
                         </button>
                     </If>
